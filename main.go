98c832695649ed8221ec35fbbd6dd1265586dd2e
felixie <1873887255@qq.com>
2024-02-18 12:07:54 +0800

    version 1.0

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func NewRouter() *gin.Engine {
 }
 
 func InitLog() *log.Logger {
-	f, err := os.OpenFile("company.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	f, err := os.OpenFile("./log/company.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("open file error: %v", err)
 	}
